fd3f522a17490bcd52fc5860b6040085fce089e2
BenjaminTruckenbrod <btrucker94@gmail.com>
2022-05-26 12:10:41 -0400

    Allow only one decimal point in a number

--- js-file.js
+++ js-file.js
@@ -26,7 +26,7 @@ function operate(operator, arr){
 }
 
 function nextOperation(){
-    testArr=[]
+    dispArr=[]
     twoNumArr=[result]
 }
 
@@ -40,35 +40,54 @@ let result = 0;
 let twoNumArr = []
 let op = "";
 let disp = ""
-let testArr= [];
+let dispArr= [];
 
+
+var percent = document.getElementById('percent')
+percent.addEventListener('click', function(event){
+    displayVal = displayVal/100
+    disp = displayVal
+    screen.innerText=disp
+
+})
+
+var plusMinus = document.getElementById("plus-minus")
+plusMinus.addEventListener('click', function(event){
+    displayVal = displayVal*(-1)
+    disp = displayVal
+    screen.innerText=disp
+})
+
+//block for typing in digits to the calculator
 var digits = document.getElementsByClassName('digits');
 for(var i = 0; i < digits.length; i++){
     digits[i].addEventListener('click', function(event){
-        // console.log(this.id);
-        console.log(this.innerText)
-        testArr.push(this.innerText)
-        disp=""
-        for(let j = 0; j<testArr.length; j++){
-            disp=disp+testArr[j]
+        if (dispArr.includes('.') && this.id == 'dot'){
         }
-        displayVal=this.innerText
-        screen.innerText=disp
-        //twoNumArr.push(parseInt(this.id))
-    
+        else{
+            console.log(this.innerText)
+            dispArr.push(this.innerText)
+            disp=""
+            for(let j = 0; j<dispArr.length; j++){
+                disp=disp+dispArr[j]
+            }
+            displayVal=this.innerText
+            screen.innerText=disp
+        }
     });
 }
 
+//block handles when operators are pushed
 var ops = document.getElementsByClassName('operator');
 for(var i = 0; i < ops.length; i++){
     // console: print the clicked <p> element
     ops[i].addEventListener('click', function(event){
-        testArr=[]
+        dispArr=[]
         twoNumArr.push(parseFloat(disp))
         
         //case for performing operations on multiple numbers without hitting "="
         if (twoNumArr.length == 2){
-            result= operate(op,twoNumArr)
+            result= round(operate(op,twoNumArr))
             displayVal=result
             screen.innerText=displayVal
             twoNumArr = [result]
@@ -81,23 +100,22 @@ for(var i = 0; i < ops.length; i++){
     });
 }
 
-
+//block for when the equals sign is pushed
 let equals = document.getElementById("equals");
 equals.addEventListener("click", function(event) {
     twoNumArr.push(parseFloat(disp))
     result = round(operate(op,twoNumArr))
     displayVal = result
     screen.innerText = displayVal
-    // testArr=[]
-    // twoNumArr=[result]
     nextOperation();
 })
 
+//block for clearing the calculator
 let clear = document.getElementById("clear")
 clear.addEventListener("click", function(event){
     screen.innerText=0
     displayVal=0
     twoNumArr=[]
     disp=""
-    testArr=[]
+    dispArr=[]
 })
